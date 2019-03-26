5b7edea55b58964532e3c5d594c1904a179b440c
ac03582 <ac03582@5CG44506GQ.ctl.intranet>
2019-03-26 11:02:57 +0530

    ss

--- SpringBoot-Heroku-Apps/src/main/java/app/restful/webservice/controller/ApplicationController.java
+++ SpringBoot-Heroku-Apps/src/main/java/app/restful/webservice/controller/ApplicationController.java
@@ -1,9 +1,18 @@
 package app.restful.webservice.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.assertj.core.util.Arrays;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import app.restful.webservice.model.EmpPhoneNum;
+import app.restful.webservice.model.Employee;
 
 @RestController
 @RequestMapping("/app")
@@ -19,6 +28,31 @@ public class ApplicationController {
 		return "Welcome : " + user;
 	}
 	
+	@GetMapping("/user/getEmp")
+	public Employee getEmp() {
+		return empLoad();
+	}
 	
+	@PostMapping("/user/Emp")
+	public Employee getEmployee(@RequestBody Employee e) {
+		System.out.println("Employee Recieved");
+		return empLoad();
+	}
 
+	
+	private Employee empLoad() {
+		EmpPhoneNum ph1 = new EmpPhoneNum();
+		ph1.setEmpPhone("1234");
+		
+		EmpPhoneNum ph2 = new EmpPhoneNum();
+		ph2.setEmpPhone("2345");
+		
+		List<EmpPhoneNum> ll = new ArrayList<>();
+		ll.add(ph1);
+		ll.add(ph2);
+		
+		Employee e = new Employee(1, "Ashish", "Module Lead", "Centurylink",ll);
+		return e;
+	}
+	
 }
